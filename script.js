5f59fcc652a0e8e21d1f352bf82e01ccfee992ce
mjstthomas0516 <mjstthomas0516@gmail.com>
2019-09-25 19:22:18 -0700

    added color

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-$('.grid').mouseenter(function(){
-	$(this).css("background", "black")
-});
+
+var selected = document.querySelector('.selected');
+var mode = document.querySelector('.mode');
+
+
 $('button').on('click', function(){
 	$('.grid').css('background', 'rgb(200, 200, 200)')
 })
+$('#black').on('click', function(){
+	$('.grid').css('background', 'rgb(200, 200, 200)');
+	black();
+})
+$('#rainbow').on('click', function(){
+	$('.grid').css('background', 'rgb(200, 200, 200)');
+	rainbow();
+})
+
+function black(){
+$('.grid').mouseenter(function(){
+	$(this).css("background", "black")
+	});
+}
+
+function rainbow(){
+	$('.grid').mouseenter(function(){
+		var r = Math.floor(Math.random()*256);
+		var g = Math.floor(Math.random()*256);
+		var b = Math.floor(Math.random()*256);
+		var colors = 'rgb(' + r + ', '+ g + ', '+ b + ')';
+	$(this).css("background", colors)
+	});
+}
+
+
+
+/*var input1 = Number(input);
+
+function grid(){
+	for(i = 0; i <= input1; i++){
+		$('.container').addElement('div .grid');
+	}
+}*/
